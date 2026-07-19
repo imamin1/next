f453e699fa0f5603fb38a9a4e9f177ba768d2a18
mohammad amin <mohammadaminshirzad09@gmail.com>
2026-07-20 01:05:48 +0330

    #6 event handling & delete User

--- app/components/Index.jsx
+++ app/components/Index.jsx
@@ -1,22 +1,27 @@
 "use client";
+import { useState } from "react";
 import UserInfo from "./UserInfo";
 
 const index = () => {
-  const userInfo = [
+  const initialUsers = [
     { name: "ali", age: 22, email: "alihossseini82@gmail.com" },
     { name: "hasan", age: 33, email: "hasanhossseini82@gmail.com" },
     { name: "hossein", age: 32, email: "hosseinhossseini82@gmail.com" },
     { name: "javad", age: 26, email: "javadhossseini82@gmail.com" },
   ];
+  const [users,setUsers] = useState(initialUsers);
+  const handleDelete = (name) => { 
+    const newUser = users.filter((user) => user.name !==name)
+    setUsers(newUser)
+  }
   return (
     <div>
-      {userInfo.map((user) => {
+      {users.map((user) => {
         return (
           <UserInfo
             key={user.name}
-            name={user.name}
-            age={user.age}
-            email={user.email}
+            {...user}
+            handleDelete={handleDelete}
           />
         );
       })}
--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const UserInfo = ({name,age,email}) => {
+const UserInfo = ({name,age,email,handleDelete}) => {
     return (
         <div>
-            <ul>
+            <ul >
                 <li>name : {name}</li>
                 <li>age : {age}</li>
                 <li>email : {email}</li>
             </ul>
+            <button onClick={()=>handleDelete(name)}>click me</button>
         </div>
     );
 };
