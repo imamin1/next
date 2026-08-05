6bf1110ef923d9b82261bde651d14626a587eed5
mohammad amin <mohammadaminshirzad09@gmail.com>
2026-08-05 15:31:35 +0330

    #24 useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import UserContextProvider from "./UserContext";
-import UserList from "./UserList";
+import React from 'react';
+import NormalComponent from './NormalComponent';
 
-function App() {
+const App = () => {
   return (
-    <UserContextProvider>
-      <div className="max-w-5xl mx-auto p-4 h-screen">
-        <UserList />
-        <div className="fixed left-0 right-0 botton-0 bg-blue-200 text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo porro necessitatibus recusandae assumenda magni nisi eaque molestias quo dolorem? Deleniti aliquam et assumenda provident laudantium earum minus vitae ea obcaecati!</div>
-      </div>
-    </UserContextProvider>
+    <div>
+      <NormalComponent/>
+    </div>
   );
-}
+};
 
 export default App;
